52627cbca0ecb51815bab157651bfd99d6bd34a3
Fernando Pinage <luizfernandoluck@hotmail.com>
2022-01-15 17:09:38 -0300

    ok

--- server.js
+++ server.js
@@ -5,16 +5,26 @@ const Port = process.env.Port || '3000';
 const config = require('./config/defaut.json')
 const bodyParser  = require("body-parser");
 
-//app.use(bodyparser.json())
-
+app.use(bodyParser.json())
 app.get('/',(req,res)=>{
 
     res.status(200).send('ok');
 
 })
 
+app.get('/api/filmes',(req,res)=>{
+
+    const filmes =[
+       {nome:'Os Vingadores 3'},
+       {nome:'Destacamento Blood'},
+       {nome:'Pantera Negra'},
+    ]
+    res.send(JSON.stringify(filmes))
+
+
+});
 
 app.listen(Port,()=>{
-    console.log('Conectado')
+    console.log('http://localhost:3000/')
 });
 
